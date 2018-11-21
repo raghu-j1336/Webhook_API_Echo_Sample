8faafd391ea45c350540fd37ec87ea36006b7e6a
raghu-j1336 <raghu.j1336@gmail.com>
2018-11-21 15:35:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
+        fulfillmentText: speech,
+    source: "EchoService"
   });
 });
 
