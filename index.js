90cdf6f097de5f6a1173c9c2474ac5d5d706c760
raghu-j1336 <raghu.j1336@gmail.com>
2018-11-21 17:15:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
     req.body.result.parameters &&
-    req.body.result.parameters.echoText
+    req.body.queryResult.parameters.echoText
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
