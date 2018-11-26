4b7813d9c0b7c46f511c6a8556013ef01b24d227
raghu-j1336 <raghu.j1336@gmail.com>
2018-11-26 17:39:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-//const request = require('request');
+const request = require('request');
 const restService = express();
 
 /*var getWeatherInfo = city =>
